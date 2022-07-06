aab6eaec6be87ba2b29ed5f098d204e1ddd34589
Ignacio Castelló <igcasseg@gmail.com>
2022-07-07 01:01:25 +0200

    Minor changes.

--- features/common-ui/src/main/java/com/simplecrud/commonui/extensions/IntExt.kt
+++ features/common-ui/src/main/java/com/simplecrud/commonui/extensions/IntExt.kt
@@ -1,6 +1,11 @@
 package com.simplecrud.commonui.extensions
 
+import android.content.res.Resources
 import com.example.commonui.R
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.UnsupportedEncodingException
 
 fun Int.toMonthResource(): Int =
     when (this) {
@@ -18,3 +23,41 @@ fun Int.toMonthResource(): Int =
         12 -> R.string.december
         else -> R.string.error
     }
+
+fun Int.getStringFromRawResource(resources: Resources): String? {
+    val inputStream: InputStream
+    inputStream = try {
+        resources.openRawResource(this)
+    } catch (e: Resources.NotFoundException) {
+        e.printStackTrace()
+        return null
+    }
+
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    val buffer = ByteArray(1024)
+    var length: Int
+    try {
+        while (inputStream.read(buffer).also { length = it } != -1) {
+            byteArrayOutputStream.write(buffer, 0, length)
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+        return null
+    } finally {
+        try {
+            inputStream.close()
+            byteArrayOutputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    val resultString: String = try {
+        byteArrayOutputStream.toString("UTF-8")
+    } catch (e: UnsupportedEncodingException) {
+        e.printStackTrace()
+        return null
+    }
+
+    return resultString
+}
